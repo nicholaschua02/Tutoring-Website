6e8494d9054eb3ebc9682b52c88ee7afa4e5bf97
Ankush Dugar <ankushdugar@gmail.com>
2024-02-18 22:02:13 +0800

    Added mouse effect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AnimatedCursor from "react-animated-cursor"
 
 // import containers (the different website pages)
 import MainPage from './containers/MainPage';
@@ -12,14 +13,19 @@ import BecomeAClient from './containers/BecomeAClient';
 
 export default function App() {
   return (
-    <Router>
-        <Routes>
-        <Route exact path="/" element={<MainPage/>} />
-        <Route exact path="/services" element={<OurServices/>} />
-        <Route exact path="/tutors" element={<OurTutors/>} />
-        <Route exact path="/become-tutor" element={<BecomeATutor/>} />
-        <Route exact path="/become-client" element={<BecomeAClient/>} />
-        </Routes>
-    </Router>
+    <div className='App'>
+      <Router>
+      <AnimatedCursor
+        color="0,0,0"
+      />
+          <Routes>
+            <Route exact path="/" element={<MainPage/>} />
+            <Route exact path="/services" element={<OurServices/>} />
+            <Route exact path="/tutors" element={<OurTutors/>} />
+            <Route exact path="/become-tutor" element={<BecomeATutor/>} />
+            <Route exact path="/become-client" element={<BecomeAClient/>} />
+          </Routes>
+      </Router>
+    </div>
   )
 }
